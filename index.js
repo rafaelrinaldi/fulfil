03b7a907fe05657be8d0407cba7e50ade705aea7
Rafael Rinaldi <rafael@rinaldi.io>
2015-08-17 11:33:41 -0300

    Added first implementation.

--- index.js
+++ index.js
@@ -2,8 +2,27 @@
 
 // TODO: https://www.npmjs.com/package/levenshtein
 
+var Levenshtein = require('levenshtein');
+
+function distance(word, comparison) {
+  return new Levenshtein(word, comparison).distance;
+}
+
 function woops(input, suggestions) {
-  return input;
+  var matches = [];
+
+  suggestions.forEach(function(suggestion) {
+    matches.push({
+      suggestion: suggestion,
+      distance: distance(input, suggestion)
+    });
+  });
+
+  matches.sort(function(current, previous) {
+    return current.distance - previous.distance;
+  });
+
+  return matches[0].suggestion;
 }
 
 module.exports = woops;
--- test.js
+++ test.js
@@ -6,5 +6,6 @@ var words = require('superb').words;
 
 test('woops', function(t) {
   t.deepEqual(woops('batueiful', words), 'beautiful', 'should suggest best match for the given input');
+  t.deepEqual(woops('awesome', words), 'awesome', 'when there is a perfect match, it should be suggested');
   t.end();
 });
